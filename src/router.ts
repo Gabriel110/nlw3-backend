a1d7965a084395e59e5684eee040e4da2614e6ad
Gabriel110 <gabrielcorreiadev@gmail.com>
2020-11-02 01:59:02 -0300

    Api completa

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import Ophanages from './controllers/OphanegeController';
+import multer from 'multer';
+import uploadConf from './config/upload';
 
 const router = express.Router();
+const upload = multer(uploadConf);
 
 router.get('/',(req, res)=>{
   return res.json({messagem:"Rota de teste!"});
 })
+
+router.post('/orphanages',upload.array('images'),Ophanages.create)
+      .get('/orphanages',Ophanages.index)
+      .get('/orphanages/:id',Ophanages.show)
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express  from 'express'; 
 import router from './router';
+import path from 'path';
+import 'express-async-errors';
+import './database/connection';
+import erroHandler from './errors/handler';
+import cors from 'cors';
 
 const port = 3333 || process.env.PORT;
 const app = express();
@@ -7,7 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(router);
-
+app.use('/uploads',express.static(path.join(__dirname,'..','uploads')));
+app.use(erroHandler);
+app.use(cors);
 app.listen(port,()=>{
   console.log(`Api rodando na porta:${port}`);
 })
