6bb3666274ebf45b5a2eaa88d3778279b830675a
Gabriel110 <gabrielcorreiadev@gmail.com>
2020-11-02 02:00:05 -0300

    Api completa

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(router);
 app.use('/uploads',express.static(path.join(__dirname,'..','uploads')));
 app.use(erroHandler);
-app.use(cors);
+app.use(cors());
 app.listen(port,()=>{
   console.log(`Api rodando na porta:${port}`);
 })
